28e2f2d07dd08df201dd0673a63eb495417f2165
RUQAYYAH MUSTAFA <ruqayyahmustafa@RUQAYYAHs-MacBook-Pro.local>
2022-03-09 13:43:53 -0500

    Adds classes, constructors, and student info file to be printed

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,9 +1,44 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("/Users/ruqayyahmustafa/IdeaProjects/Zoo/src/com/company/StudentInfo");
+        Scanner studentFile = new Scanner(file);
+        Scanner usrInput = new Scanner(System.in);
+        List<studentInfo> studentInfoList = new ArrayList<studentInfo>();
+        List<String> classes = new ArrayList<>();
+        List<String> grades = new ArrayList<>();
+        int i = 0;
+        while(studentFile.hasNext()){
+            System.out.println("This is where the loop starts");
+            String studentID = studentFile.next();
+            String email = studentFile.next();
+            String password = studentFile.next();
+            String lastName = studentFile.next();
+            String firstName = studentFile.next();
+            double currentGPA = studentFile.nextDouble();
+            for(int j = 0; j<3; j++) {
+                classes.add(j, studentFile.next());
+                grades.add(j, studentFile.next());
+            }//end for i
+            double tuition = studentFile.nextInt();
 
+            studentInfoList.add(i, new studentInfo(studentID, email,password, lastName, firstName,
+                                currentGPA, Collections.singletonList(classes.get(i)),
+                                Collections.singletonList(grades.get(i)), tuition));
+            studentInfoList.get(i).printStudentInfo();
+            i++;
+
+
+        }
 
     }//end main
 }//end Main
---
+++ src/main/java/com/company/studentInfo.java
@@ -0,0 +1,79 @@
+package com.company;
+import java.util.*;
+
+public class studentInfo {
+    private String studentId;
+    private String email;
+    private String password;
+    private String lastName;
+    private String firstName;
+    private double currentGPA;
+    private List<String> classes;
+    private List<String> grades;
+    private double tuition;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getLastName(String lastName){
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public double getCurrentGPA() {
+        return currentGPA;
+    }
+
+    public List<String> getClasses() {
+        return classes;
+    }
+
+    public void setGrades(List<String> grades) {
+        this.grades = grades;
+    }
+
+    public double getTuition() {
+        return tuition;
+    }
+
+    public studentInfo(String studentId, String email, String password, String lastName, String firstName,
+                       double currentGPA, List<String> classes, List<String> grades, double tuition)
+    {
+        this.studentId = studentId;
+        this.email = email;
+        this.password = password;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.currentGPA = currentGPA;
+        this.classes = classes;
+        this.grades = grades;
+        this.tuition = tuition;
+    }
+
+    public void printStudentInfo()
+    {
+        System.out.println("Last Name: " + lastName + '\n' +
+                "First name: " + firstName +'\n' +
+                "Student ID: " + studentId + '\n' +
+                "Email Address: " + email + '\n' +
+                "Classes \n" +
+                "1. " + classes + " - " + grades + '\n' +
+                "2. " + classes + " - " + grades + '\n' +
+                "3. " + classes + " - " + grades + '\n' +
+                "GPA: " + currentGPA +'\n' +
+                "Tuition: " + tuition);
+    }
+}//end studentInfo Class
+
